a6f2860c25b8a89a9f848786100264d30ca099e2
naomimachado21 <naomimachado21@gmail.com>
2018-01-26 20:36:43 -0500

    updated

--- assets/js/demo.jsx
+++ assets/js/demo.jsx
@@ -3,48 +3,97 @@ import ReactDOM from 'react-dom';
 import { Button } from 'reactstrap';
 
 export default function run_demo(root) {
-  ReactDOM.render(<Demo side={0}/>, root);
+  ReactDOM.render(<Layout />, root);
 }
 
-class Demo extends React.Component {
-  constructor(props) {
+const gameStates = {FC:"First",SC:"Second"};
+
+class Card extends React.Component{
+  render(){
+    return <div className="card"><span>{this.props.card.flipped?this.props.card.cardValue:"?"}</span>
+  </div>
+}
+}
+
+class Layout extends React.Component {
+
+  constructor(props){
     super(props);
-    this.state = { side: props.side };
+    this.state={cards: initialize(), game: gameStates.FC, firstCard: null, count: 0};
   }
 
-  toggle(side) {
-    var side = +!this.state.side;
-    this.setState({side: side});
+  clickCard(card) {
+    if(!card.flipped){
+      switch (this.state.game) {
+        case gameStates.FC:
+        this.state.cards[card.rowIndex][card.columnIndex].flipped = true;
+        this.setState({cards: this.state.cards, firstCard: card, game: gameStates.SC, count: this.state.count + 1});
+        break;
+        case gameStates.SC:
+        this.state.cards[card.rowIndex][card.columnIndex].flipped = true;
+        this.setState({game: gameStates.FC, cards: this.state.cards, count: this.state.count + 1});
+        if (this.state.firstCard.cardValue != card.cardValue) {
+          setTimeout(()=>{
+            this.state.cards[this.state.firstCard.rowIndex][this.state.firstCard.columnIndex].flipped = false;
+            this.state.cards[card.rowIndex][card.columnIndex].flipped = false;
+            this.setState({game: gameStates.FC,firstCard: null, cards: this.state.cards});
+          }, 500);
+        } else {
+          setTimeout(()=>{
+            this.state.cards[this.state.firstCard.rowIndex][this.state.firstCard.columnIndex].cardValue = "MATCH";
+            this.state.cards[card.rowIndex][card.columnIndex].cardValue = "MATCH";
+            this.setState({game: gameStates.FC,firstCard: null, cards: this.state.cards});
+          }, 500);
+        }
+        break;
+      }
+    }
+}
+
+  restore(){
+    this.setState({cards: initialize(), game: gameStates.FC, firstCard: null, secondCard: null, count: 0});
   }
 
   render() {
-    var toggle = this.toggle.bind(this);
-    return (
-      <div className="row">
-        <Side show={this.state.side == 0} toggle={toggle} />
-        <div className="col">
-          &nbsp;
-        </div>
-        <Side show={this.state.side == 1} toggle={toggle} />
-      </div>
-    );
-  }
+    const cardsRendered = this.state.cards.map((rowOfCards, rowIndex)=><tr>{rowOfCards.map((card, indexOfCardRow)=><td onClick={()=>this.clickCard(card)}><Card card={card}/></td>)}
+  </tr>);
+  return <div><table><tbody>{cardsRendered}</tbody></table><div><br></br></div>
+  <div><p>No. of clicks:<b>{this.state.count}</b></p></div><button onClick={()=>this.restore()}>RESET</button></div>
+}
 }
 
-function Side(params) {
-  if (params.show) {
-    return (
-      <div id="side-0" className="side col" onMouseOver={ () => params.toggle() }>
-        <Button onClick={ () => alert("cheater") }>Click Me</Button>
-      </div>
-    );
+function initialize(){
+  var cards = createArray(4,4);
+  var data = ['A','B','C','D','E','F','G','H','A','B','C','D','E','F','G','H'];
+  data = shuffle(data);
+  var k = 0;
+  for(var i=0; i<4; i++){
+    for(var j=0; j<4; j++){
+      cards[i][j] = {cardValue: data[k], flipped:false, rowIndex:i, columnIndex:j};
+      k++;
+    }
   }
-  else {
-    return (
-      <div id="side-0" className="side col">
-        &nbsp;
-      </div>
-    );
-  }
+  return cards;
 }
 
+function createArray(x, y) {
+  return Array.apply(null, Array(x)).map(function(e) {
+    return Array(y);
+  });
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
